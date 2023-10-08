ab33afb2c9292e840122f1978edbc4482a97ff09
Nguyen Dinh Hoang Anh <ndha1115@gmail.com>
2023-10-08 21:21:31 +0700

    create project

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   return (
